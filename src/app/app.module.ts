b6cb909036bc0257ac83b2eaa6a362e0808c02ec
Kinga Biernacka <kingabiernacka@op.pl>
2017-04-27 20:57:22 +0200

    ng routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PictureNewsComponent } from './picture-news/picture-news.component';
 import { ListNewsComponent } from './list-news/list-news.component';
+import { SingleArticleComponent } from './single-article/single-article.component';
+
+const appRoutes: Routes = [
+  { path: '', component: PictureNewsComponent },
+  { path: 'news', component: SingleArticleComponent }
+];
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PictureNewsComponent,
-    ListNewsComponent
+    ListNewsComponent,
+    SingleArticleComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
