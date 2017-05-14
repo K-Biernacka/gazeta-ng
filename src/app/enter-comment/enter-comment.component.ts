2d30356fe06e728dbbfc5cc6ef63a1a13c3e8d6d
Kinga Biernacka <kingabiernacka@op.pl>
2017-05-14 22:42:31 +0200

    save comment to localstorage

--- src/app/enter-comment/enter-comment.component.ts
+++ src/app/enter-comment/enter-comment.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnterCommentComponent implements OnInit {
 
+
+  payload={
+    comment: '',
+    userName: ''
+  };
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+  onSubmit() {
+  let comments: any = localStorage.getItem("comments");
+
+  comments = JSON.parse(comments);
+  if (comments == null) {
+    comments = []
+  }
+  comments.unshift(this.payload);
+  localStorage.setItem("comments", JSON.stringify(comments));
+
+
+
   }
 
 }
